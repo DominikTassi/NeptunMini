86ca499148b41ff7a23ba6772e35feae5b810954
Tassi Dominik <doma_tassi@hotmail.com>
2018-05-16 15:19:49 +0200

    last push for today from work

--- src/main/java/NeptunMini/repository/RegisteredSubjectRepository.java
+++ src/main/java/NeptunMini/repository/RegisteredSubjectRepository.java
@@ -2,6 +2,8 @@ package NeptunMini.repository;
 
 import NeptunMini.entity.RegisteredSubject;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RegisteredSubjectRepository extends CrudRepository<RegisteredSubject, Integer> {
 }
