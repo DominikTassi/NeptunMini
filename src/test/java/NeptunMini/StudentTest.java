a0a0b1e037c8e94b5d42a234b8e9c5ce3507f3d5
Tassi Dominik <doma_tassi@hotmail.com>
2018-05-23 13:14:31 +0200

    added StudentTest

--- src/test/java/NeptunMini/StudentTest.java
+++ src/test/java/NeptunMini/StudentTest.java
@@ -1,14 +1,20 @@
 package NeptunMini;
 
+import NeptunMini.entity.RegisteredSubject;
 import NeptunMini.entity.Student;
+import NeptunMini.entity.Subject;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class StudentTest {
     private Student testStudent = null;
+    private List<RegisteredSubject> registeredSubjects = new ArrayList<>();
 
     @Before
     public void setUp() throws Exception {
@@ -39,6 +45,21 @@ public class StudentTest {
     }
 
 
+    @Test
+    public void setRegisteredSubjects() throws Exception{
+        List<RegisteredSubject> localregsub = new ArrayList<>();
+        localregsub.add(new RegisteredSubject(new Subject("GEAIL-123B", "OS", 5), 2));
+        testStudent.setRegisteredSubjects(localregsub);
+        assertEquals(localregsub, testStudent.getRegisteredSubjects());
+    }
+
+
+    @Test
+    public void addRegistratedSubject() throws Exception{
+        RegisteredSubject localregsub = new RegisteredSubject(new Subject("GEAIL-123B", "OS", 5), 2);
+        testStudent.addRegisteredSubjects(localregsub);
+        assertEquals(localregsub, testStudent.getRegisteredSubjects().get(0));
+    }
 
 
 }
